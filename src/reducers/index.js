ff27b8d70937e6ebed13388dbbd2a27628e841ae
Ferdinand Ismael <ferdinandismael@Ferdinands-MacBook-Pro.local>
2018-04-24 09:02:59 -0400

    deleted unnecssary files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import lineReducer from "./lineReducer";
 
+//we use combineReducers in order to merge all of our other reducer components
+//unforotunately there is only one
 export default combineReducers({
    lines: lineReducer
 });
